4784bcfa2b572a8122aefa07e01c5cc16977c84e
Kseniia Tikhonova <tikhonovakb@gmail.com>
2023-01-03 17:07:34 +0100

    hotfix: исправить ошибки первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,43 @@
 // выборка DOM элементов
-const popupElement = document.querySelector(".popup");
-const formCloseButtonElement = popupElement.querySelector(".popup__button-close");
-const profileEditButtonElement = document.querySelector(".profile__edit-button");
+const popupElement = document.querySelector('.popup');
+const popupCloseButton = popupElement.querySelector('.popup__button-close');
+const profileEditButtonElement = document.querySelector('.profile__edit-button');
 
 // Находим форму в DOM
-const formElement = document.querySelector(".popup__profile-editing");
+const formElement = document.querySelector('.popup__form');
+
 // Находим поля формы в DOM
-const nameInput = popupElement.querySelector(".popup__input-name");
-const jobInput = popupElement.querySelector(".popup__input-job");
-const nameTitle = document.querySelector(".profile__name");
-const jobTitle = document.querySelector(".profile__job");
+const nameInput = popupElement.querySelector('.popup__input_data_name');
+const jobInput = popupElement.querySelector('.popup__input_data_job');
+const nameTitle = document.querySelector('.profile__name');
+const jobTitle = document.querySelector('.profile__job');
 
-const openPopup = function() {
-   popupElement.classList.add("popup_is-opened");
+const openPopup = function () {
+  const nameTitleValue = nameTitle.textContent;
+  nameInput.value = nameTitleValue;
 
-   const nameTitleValue = nameTitle.textContent;
-   nameInput.value = nameTitleValue;
-
-   const jobTitleValue = jobTitle.textContent;
-   jobInput.value = jobTitleValue;
-   }
+  const jobTitleValue = jobTitle.textContent;
+  jobInput.value = jobTitleValue;
+  popupElement.classList.add('popup_is-opened');
+};
 
 const closePopup = function () {
-    popupElement.classList.remove("popup_is-opened");
-  }
+  popupElement.classList.remove('popup_is-opened');
+};
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
 
   const nameValue = nameInput.value;
   nameTitle.textContent = nameValue;
 
-  const jobValue=jobInput.value;
-  jobTitle.textContent=jobValue;
-    
+  const jobValue = jobInput.value;
+  jobTitle.textContent = jobValue;
+
   closePopup();
-  } 
+}
 
 // регистрация обработчиков событий по клику
-profileEditButtonElement.addEventListener("click", openPopup);
-formCloseButtonElement.addEventListener("click", closePopup);
-formElement.addEventListener('submit', handleFormSubmit); 
+profileEditButtonElement.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
