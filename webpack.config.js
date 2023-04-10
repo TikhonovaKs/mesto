71549ee070cb50860ec56fbfa9b644c0d4a70ab9
Kseniia Tikhonova <tikhonovakb@gmail.com>
2023-04-10 14:42:12 +0200

    hotfix: fixing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: '/src/index.html',
+      template: './src/index.html',
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
